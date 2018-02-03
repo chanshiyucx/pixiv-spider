2eb2f1edcdfe5724a91f06525dd942eee6baf9a1
chanshiyu <1124590931@qq.com>
2018-02-03 18:56:40 +0800

    添加食用说明

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  username: '1124590931@qq.com',
-  password: '421963',
-  mode: 'star', // star: 下载收藏夹, author: 下载作者列表, follow: 下载我关注的作者
+  username: '你的 P 站账号',
+  password: '你的 P 站密码',
+  mode: 'author', // star: 下载收藏夹, author: 下载作者列表, follow: 下载我关注的作者
   date: '', // 限定日期之内, 可以留空, 注意格式: '2018/01/01'
   // 以下是 mode 为 author 或 follow 才有的选项
   author: [], // 下载的作者列表
